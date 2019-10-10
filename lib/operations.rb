e3a5d9c80da432a09e40d1a7c2f29e05642c62d2
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-10 22:45:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+     if speed > 60 || speed < 40 
+       return true
+     end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed == 0..40 ? return true : return false
+	speed == 60..100 ? return true : return false
 end
 	
 
